1ad14b839ebfeaed830ed65d705e54da5c8485ac
Dennis_Arango <dennis.arango.mt@correo.usa.edu.co>
2021-06-22 15:54:08 -0500

    Modifying folders

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+/*-------------------------------Funciones-------------------------------*/
+
+function changeIndex(){
+    location.href="../../index.html" //Función que dirige al usuario a la página principal (desde cualquier otra página)
+}
+function changePlantas(){
+    location.href="plantas.html" //Función que dirige al usuario a la página: plantas (desde cualquier otra página)
+}
+function changeArboles(){
+    location.href="arboles.html" //Función que dirige al usuario a la página: árboles (desde cualquier otra página)
+}
+function changeAsesorias(){
+    location.href="asesorias.html" //Función que dirige al usuario a la página: asesorías (desde cualquier otra página)
+}
+function changeQS(){
+    location.href="qs.html" //Función que dirige al usuario a la página: ¿quiénes somos? (desde cualquier otra página)
+}
+
+
+
+function changeIn_Index(){
+    location.href="index.html" //Función que dirige al usuario a la página principal (desde el index)
+}
+function changeIn_Plantas(){
+    location.href="./src/pages/plantas.html" //Función que dirige al usuario a la página: plantas (desde el index)
+}
+function changeIn_Arboles(){
+    location.href="./src/pages/arboles.html" //Función que dirige al usuario a la página: árboles (desde el index)
+}
+function changeIn_Asesorias(){
+    location.href="./src/pages/asesorias.html" //Función que dirige al usuario a la página: asesorías (desde el index)
+}
+function changeIn_QS(){
+    location.href="./src/pages/qs.html" //Función que dirige al usuario a la página: ¿quiénes somos? (desde el index)
+}
+
+
+
+function goToIg(){
+    location.href="https://www.instagram.com"  //Función que dirige al usuario a la cuenta de instagram de green house
+}
+function goToYb(){
+    location.href="https://www.youtube.com" //Función que dirige al usuario a la cuenta de youtube de green house
+}
