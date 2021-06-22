5435adf59b7e4767402917273c8f46b3f68f86e5
Dennis_Arango <dennis.arango.mt@correo.usa.edu.co>
2021-06-22 08:31:36 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/*-------------------------------Funciones-------------------------------*/
+
+function changeIndex(){
+    location.href="index.html" //Función que dirige al usuario a la página principal
+}
+function changePlantas(){
+    location.href="plantas.html" //Función que dirige al usuario al index
+}
+function changeArboles(){
+    location.href="arboles.html" //Función que dirige al usuario a la página: opción 1, ¿qué signo soy?
+}
+function changeAsesorias(){
+    location.href="asesorias.html" //Función que dirige al usuario a la página: opción 2, ¿cómo es mi signo?
+}
+function changeQS(){
+    location.href="qs.html" //Función que dirige al usuario a la página: opción 3, ¿estaremos juntos?
+}
